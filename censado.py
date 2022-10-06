b75457934d74f0308d33c857632e07f96039c013
Nicolas Cifuentes <nicolas.cifuentes01@correo.usa.edu.co>
2022-10-06 13:45:32 -0500

    Documentacion y test 2

--- censado.py
+++ censado.py
@@ -1,6 +1,7 @@
 import dht
 from machine import Pin
 
+# Función para verificar el funcionamiento del sensor
 def revision(conexion) :
     try :
         sensor = dht.DHT11(Pin(conexion))
@@ -9,16 +10,19 @@ def revision(conexion) :
     except OSError as e :
         return False
 
+# Función para retornar la temperatura
 def temperatura(conexion) :
     sensor = dht.DHT11(Pin(conexion))
     sensor.measure()
     return sensor.temperature()
 
+# Función para retornar la humedad
 def humedad(conexion) :
     sensor = dht.DHT11(Pin(conexion))
     sensor.measure()
     return sensor.humidity()
 
+# Función para retornar la temperatura y humedad
 def temperatura_humedad(conexion) :
     sensor = dht.DHT11(Pin(conexion))
     sensor.measure()
--- test.py
+++ test.py
@@ -1,8 +1,12 @@
 import censado
 
+# Asignación del pin usado en la ESP32
 pin = 15
 
 if censado.revision(pin) :
-    print(censado.temperatura(pin))
-    print(censado.humedad(pin))
-    print(censado.temperatura_humedad(pin))
+    temp_1 = censado.temperatura(pin)
+    hum_1 = censado.humedad(pin)
+    temp_2,hum_2 = censado.temperatura_humedad(pin)
+    print('Temperatura 1:{} C\nHumedad 1:{} %\nTemperatura 2:{} C\nHumedad 2:{} %\n'.format(temp_1,hum_1,temp_2,hum_2))
+else :
+    print('Error de censado')
