bd68aadb937554f4bc3fa8541bef1387ee90b727
Nicolas Cifuentes <nicolas.cifuentes01@correo.usa.edu.co>
2022-10-08 16:24:56 -0500

    Pulsador con sensor dht11

--- pulsado.py
+++ pulsado.py
@@ -1,13 +1,20 @@
 from machine import Pin
 import time
+import censado
 
 boton = Pin(5,Pin.IN,Pin.PULL_DOWN)
 led = Pin(2, Pin.OUT)
+sensor = 15
+
+print('INICIADO')
 
 while True :
     estado_btn = boton.value()
-    if estado_btn :
+    if estado_btn and censado.revision(sensor) :
         led.on()
+        print('Temperatura: {}'.format(censado.temperatura(sensor)))
+    elif not estado_btn and censado.revision(sensor) :
+        led.off()
+        # print(estado_btn)
     else :
-        led.off()    
-    print(estado_btn)
+        print('Error en lectura')
